388f67abb02d2eac00fb69c25dc1c3876b6350fe
noa <noa@3beca68c-c521-0410-ae18-dd2a0ae60fc9>
2007-07-11 08:42:47 +0000

    updated to new SendTask constructor parameters, uncommented
    git-svn-id: svn+ssh://svn.complicata.se/srv/svnroot/jresolver/trunk@480 3beca68c-c521-0410-ae18-dd2a0ae60fc9

--- test/src/com/voxbiblia/jresolver/ConverstationSchedulerTest.java
+++ test/src/com/voxbiblia/jresolver/ConverstationSchedulerTest.java
@@ -2,6 +2,8 @@ package com.voxbiblia.jresolver;
 
 import junit.framework.TestCase;
 
+import java.util.LinkedList;
+
 
 /**
  * Tests ConversationScheduler
@@ -11,24 +13,21 @@ public class ConverstationSchedulerTest
 {
     public void testPutSorted()
     {
-        // TODO revive
-        /*
-        ConversationScheduler.MyLinkedList l = new ConversationScheduler.MyLinkedList();
-        l.add(new ConversationScheduler.SendTask(0, 100L, null));
-        l.add(new ConversationScheduler.SendTask(0, 200L, null));
-        l.add(new ConversationScheduler.SendTask(0, 300L, null));
+        LinkedList l = new LinkedList();
+        l.add(new ConversationScheduler.SendTask(100L, null));
+        l.add(new ConversationScheduler.SendTask(200L, null));
+        l.add(new ConversationScheduler.SendTask(300L, null));
 
-        ConversationScheduler.putSorted(l, new ConversationScheduler.SendTask(0, 150L, null));
+        ConversationScheduler.putSorted(l, new ConversationScheduler.SendTask(150L, null));
         assertEquals(4, l.size());
         assertEquals(150L, ((ConversationScheduler.SendTask)l.get(1)).getTime());
 
         l.remove(1);
-        ConversationScheduler.putSorted(l, new ConversationScheduler.SendTask(0, 50L, null));
+        ConversationScheduler.putSorted(l, new ConversationScheduler.SendTask(50L, null));
         assertEquals(50L, ((ConversationScheduler.SendTask)l.get(0)).getTime());
 
-        ConversationScheduler.putSorted(l, new ConversationScheduler.SendTask(0, 400L, null));
+        ConversationScheduler.putSorted(l, new ConversationScheduler.SendTask(400L, null));
         assertEquals(5, l.size());
         assertEquals(400L, ((ConversationScheduler.SendTask)l.get(4)).getTime());
-        */
     }
 }
