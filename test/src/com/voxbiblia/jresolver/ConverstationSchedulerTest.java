4545967491d056f65ed77e1f1b647cd3cdefa576
noa <noa@3beca68c-c521-0410-ae18-dd2a0ae60fc9>
2007-07-11 16:03:31 +0000

    added README, build file and GPL3 Copyright notices
    git-svn-id: svn+ssh://svn.complicata.se/srv/svnroot/jresolver/trunk@481 3beca68c-c521-0410-ae18-dd2a0ae60fc9

--- test/src/com/voxbiblia/jresolver/ConverstationSchedulerTest.java
+++ test/src/com/voxbiblia/jresolver/ConverstationSchedulerTest.java
@@ -1,3 +1,18 @@
+/*
+jresolver - The java DNS resolver library
+Copyright (C) 2007  Noa Resare (noa@voxbiblia.com)
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+The GNU General Public License is available from <http://gnu.org/licenses/>.
+*/
 package com.voxbiblia.jresolver;
 
 import junit.framework.TestCase;
--- test/src/com/voxbiblia/jresolver/ThreadingTest.java
+++ test/src/com/voxbiblia/jresolver/ThreadingTest.java
@@ -1,3 +1,18 @@
+/*
+jresolver - The java DNS resolver library
+Copyright (C) 2007  Noa Resare (noa@voxbiblia.com)
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+The GNU General Public License is available from <http://gnu.org/licenses/>.
+*/
 package com.voxbiblia.jresolver;
 
 /**
@@ -16,7 +31,6 @@ public class ThreadingTest
                 //Thread.sleep(1000);
                 System.out.println("Waiter: done sleeping");
             //} catch (InterruptedException e) {
-            //    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             //}
             System.out.println("Waiter: waiting");
             try {
