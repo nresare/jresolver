eb2dadb92008a340e6bfb60ed98387b4f7c6f206
Noa Resare <noa@resare.com>
2009-02-26 18:07:06 +0100

    Added handling of REFUSED status with QueryRefusedException

---
+++ src/com/voxbiblia/jresolver/QueryRefusedException.java
@@ -0,0 +1,9 @@
+package com.voxbiblia.jresolver;
+
+/**
+ * This exception is thrown when the status returned from the server is REFUSED
+ */
+public class QueryRefusedException
+    extends RuntimeException
+{
+}
